ed120344784060afd55969ba380bd37dc5a23d8f
Michael Freeman <michaeljrfreeman@gmail.com>
2023-04-07 22:58:57 +0000

    added localhost

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
+      host: env('PGHOST', 'localhost'),
       port: env.int('PGPORT', 5432),
       database: env('PGDATABASE', 'strapi'),
       user: env('PGUSER', 'strapi'),
