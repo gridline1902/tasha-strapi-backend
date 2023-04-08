5024a48971dbdd0f969b6cfb29329680e026775f
Michael Freeman <michaeljrfreeman@gmail.com>
2023-04-08 03:05:07 +0000

    category-images mapped and indexed

---
+++ src/api/category-image/controllers/category-image.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * category-image controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::category-image.category-image');
---
+++ src/api/category-image/services/category-image.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * category-image service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::category-image.category-image');
